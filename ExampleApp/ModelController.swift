a85bf6b3f6a891bb1658bee6eca2274fa771611e
Ben Yule <bjyule@gmail.com>
2015-10-11 08:54:49 -0700

    Restructuring and adding Travis config

--- ExampleApp/ModelController.swift
+++ ExampleApp/ModelController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import Alamofire
 
 /*
  A controller object that manages a simple model -- a collection of month names.
  
  The controller serves as the data source for the page view controller; it therefore implements pageViewController:viewControllerBeforeViewController: and pageViewController:viewControllerAfterViewController:.
- It also implements a custom method, viewControllerAtIndex: which is useful in the implementation of the data source methods, and in the initial configuration of the application.
+ It also implements a custom method, viewControllerAtIndex: which is useful in the implementation of the data source methods, andgit@github.com:byule/TravisExamples.git in the initial configuration of the application.
  
  There is no need to actually create view controllers for each page in advance -- indeed doing so incurs unnecessary overhead. Given the data model, these methods create, configure, and return a new view controller on demand.
  */
