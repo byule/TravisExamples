7a10f6108d7ef94527ede7ee2f7887aaacf482e3
Ben Yule <bjyule@gmail.com>
2015-10-11 08:33:56 -0700

    Initial Project Checkin

---
+++ ExampleApp/ExampleApp/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  ExampleApp
+//
+//  Created by Benjamin Yule on 10/11/15.
+//  Copyright © 2015 Spire, Inc. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
